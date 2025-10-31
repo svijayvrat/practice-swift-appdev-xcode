8e041966cc6980950566cc4bed092d2d18943786
svijayvrat <svijayvrat@outlook.com>
2025-10-31 15:01:45 +0530

    Create Apple Pie initial UI
    initial UI for Apple Pie game created

---
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by GEU on 31/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
