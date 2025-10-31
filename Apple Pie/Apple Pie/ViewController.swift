8171d7826d11d501e3f47cae7b930967b68220e7
svijayvrat <svijayvrat@outlook.com>
2025-10-31 15:57:22 +0530

    Handle a Win or Loss, Done Game
    created win/loss logic

--- Apple Pie/Apple Pie/ViewController.swift
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -11,8 +11,16 @@ class ViewController: UIViewController {
 
     var listOfWords=["buccaneer","swift","glorious","indcandescent","bug","program"]
     let incorrectMovesAllowed=7
-    var totalWins=0
-    var totalLosses=0
+    var totalWins=0{
+        didSet{
+            newRound()
+        }
+    }
+    var totalLosses=0{
+        didSet{
+            newRound()
+        }
+    }
     
     @IBOutlet var treeImageView: UIImageView!
     @IBOutlet var correctWordLabel: UILabel!
@@ -25,9 +33,19 @@ class ViewController: UIViewController {
     }
     var currentGame: Game!
     func newRound(){
-        let newWord=listOfWords.removeFirst()
-        currentGame=Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty{
+            let newWord=listOfWords.removeFirst()
+            currentGame=Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        }else{
+            enableLetterButtons(false)
+        }
+    }
+    func enableLetterButtons(_ enable: Bool){
+        for button in letterButtons{
+            button.isEnabled=enable
+        }
     }
     func updateUI(){
         var letters=[String]()
@@ -44,9 +62,17 @@ class ViewController: UIViewController {
         let letterString=sender.configuration!.title!
         let letter=Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
     }
-    
+    func updateGameState(){
+        if currentGame.incorrectMovesRemaining==0{
+            totalLosses+=1
+        }else if currentGame.word==currentGame.formattedWord{
+            totalWins+=1
+        }else{
+            updateUI()
+        }
+    }
 
 }
 
